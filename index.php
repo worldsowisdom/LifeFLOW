8ebf1bbad538c68b256de5e6898f37c684183d5f
Eagle Gamma <eagle@eaglegamma.com>
2017-05-23 20:28:41 -0400

    Add subtitle! :)
    Also a few creaturez! :)

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <html>
 <head>
 	<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
-	<title>LifeFLOW</title>
+	<title>LifeFLOW Digitarium: Grow a little bit! :)</title>
 </head>
 <body>
-<p><a href="/" id="logo" rel="home" title="Home"><img alt="LifeWOW" src="img/LifeWOW.png" />LifeFLOW<img alt="Hearts" src="img/hearts.svg" width="60"></a><a href="#About">About LifeFLOW! :)</a> - <a href="#Music">Choose Your Music! :)</a></p>
+<p><a href="/" id="logo" rel="home" title="Home"><img alt="LifeWOW" src="img/LifeWOW.png" />LifeFLOW Digitarium: Grow a little bit! :)<img alt="Hearts" src="img/hearts.svg" width="60"></a><a href="#About">About LifeFLOW! :)</a> - <a href="#Music">Choose Your Music! :)</a></p>
 <!--?xml version="1.0" encoding="UTF-8" standalone="no"?-->
 
 <table border="0" cellpadding="1" cellspacing="1" style="width: 600px;">
